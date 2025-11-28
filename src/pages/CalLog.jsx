56329606c925f4a13c0a87ef160bd7e6a05384e5
Yeison <programador15@tecnologicocomfenalco.edu.co>
2025-11-28 16:06:06 -0500

    feat: create CalLog component with PhoneLayout integration

---
+++ src/pages/CalLog.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { PhoneLayout } from '../Layout/PhoneLayout'
+
+export const CalLog = () => {
+    return (
+        <PhoneLayout>
+            
+        </PhoneLayout>
+    )
+}
